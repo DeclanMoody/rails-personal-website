8d2cc6a3c00fe769669bdcd641dd14dcdf93827b
Duane M. Moody <dmmoody@gmail.com>
2015-07-27 21:43:38 -0700

    remove mailer config settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: "declan@gmail.com"
+  # default from: "declan@gmail.com"
 
-  def registration_confirmation(user)
-    @user = user
+  # def registration_confirmation(user)
+  #   @user = user
     
-    mail to: user.email, subject: "Sign Up Confirmation"
-  end
+  #   mail to: user.email, subject: "Sign Up Confirmation"
+  # end
 end
